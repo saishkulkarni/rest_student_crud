c44f2fa7cd0c06280f1e5b8fe5cba7c009fbe183
saishkulkarni <saishkulkarni7@gmail.com>
2024-12-06 17:12:26 +0530

    done

--- src/main/java/batch/e3/student_crud/StudentCrudApplication.java
+++ src/main/java/batch/e3/student_crud/StudentCrudApplication.java
@@ -3,7 +3,12 @@ package batch.e3.student_crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Student REST API", description = "Rest api which has endpoints for performing crud operation on student table",version = "1.0.0",contact = @Contact(name = "Saish",email = "saishkulkarni7@gmail.com",url = "https://www.github.com/saishkulkarni")))
 public class StudentCrudApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/batch/e3/student_crud/controller/SwaggerController.java
@@ -0,0 +1,13 @@
+package batch.e3.student_crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerController {
+
+	@GetMapping("/")
+	public String loadSwagger() {
+		return "redirect:/swagger-ui/index.html";
+	}
+}
